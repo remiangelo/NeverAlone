81f6b080b7ff56f74d31d72fd6744aa61cbbed40
Remi <82326168+remiangelo@users.noreply.github.com>
2025-05-10 23:36:08 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  swcMinify: true,
   // Explicitly define the pages directory
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   // Explicitly set the directory structure
